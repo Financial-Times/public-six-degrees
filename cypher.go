ff455415e1fb698d11404113fb329d475110a49a
Sarah Wells <sarah.wells@ft.com>
2016-05-17 16:53:23 +0100

    First working version of connectedPerson query

--- cypher.go
+++ cypher.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/jmcvetta/neoism"
 )
 
 // Driver interface
 type Driver interface {
-	ConnectedPeople(uuid string, fromDateEpoch int64, toDateEpoch int64, limit int, minimumConnections int) (connectedPeople[] ConnectedPerson, found bool, err error)
+	ConnectedPeople(uuid string, fromDateEpoch int64, toDateEpoch int64, limit int, minimumConnections int) (connectedPeople []ConnectedPerson, found bool, err error)
 	MostMentioned(fromDateEpoch int64, toDateEpoch int64, limit int) (thingList People, found bool, err error)
 	CheckConnectivity() error
 }
@@ -39,54 +40,6 @@ func (pcw CypherDriver) CheckConnectivity() error {
 }
 
 type neoReadStruct struct {
-}
-
-//func (pcw CypherDriver) MostMentioned(fromDate string, toDate string, limit int64) (Thing, bool, error) {
-//	return Thing{"bla", "bla", "bla"}, true, nil
-//}
-
-//MATCH (c:Content) where c.publishedDateEpoch < 1460371050 and c.publishedDateEpoch > 1457605324
-//WITH c
-//MATCH (p:Person{uuid:"9421d9ee-7e0f-3f7c-8adc-ded83fabdb92"})<-[:MENTIONS]-(c)-[:MENTIONS]->(p2:Person)
-//WITH p, count(distinct(c)) as cm, p2
-//WHERE cm > 5
-//return cm, p2 limit 25
-func (pcw CypherDriver) ConnectedPeople(uuid string, fromDateEpoch int64, toDateEpoch int64, limit int, minimumConnections int) (connectedPeople[] ConnectedPerson, found bool, err error) {
-	connectedPeople = []ConnectedPerson{}
-	results := []struct {
-		Rs []neoReadStruct
-	}{}
-	statement := `
-	MATCH (c:Content) where c.publishedDateEpoch < {toDate} and c.publishedDateEpoch > {fromDate}
-	WITH c
-	MATCH (p:Person{uuid:{uuid}})<-[:MENTIONS]-(c)-[:MENTIONS]->(p2:Person)
-	WITH p, count(distinct(c)) as cm, p2
-	WHERE cm > {minimumConnections}
-	WITH p2.uuid as uuid
-	RETURN name, uuid, mentions ORDER BY mentions DESC LIMIT {limit}`
-	//thing := Thing{}
-	query := &neoism.CypherQuery{
-		Statement:  statement,
-		Parameters: neoism.Props{
-			"uuid": uuid,
-			"fromDate": fromDateEpoch,
-			"toDate": toDateEpoch,
-			"minimumConnections": minimumConnections,
-			"limit": limit,
-		},
-		Result:     &results,
-	}
-	err = pcw.db.Cypher(query)
-	if err != nil {
-		log.Errorf("Error finding %v most mentioned people between %v and %v with the following statement: %v  Error: %v", limit, fromDateEpoch, toDateEpoch, query.Statement, err)
-		return []ConnectedPerson{}, false, fmt.Errorf("Error finding %v most mentioned people between %v and %v", limit, fromDateEpoch, toDateEpoch)
-	}
-	log.Debugf("CypherResult MostMentioned was (fromDate=%v, toDate=%v): %+v", limit, fromDateEpoch, toDateEpoch, results)
-	if (len(results)) == 0 || len(results[0].Rs) == 0 {
-		return []ConnectedPerson{}, false, nil
-	}
-	log.Debugf("Returning %v", connectedPeople)
-	return connectedPeople, true, nil
 }
 
 func (pcw CypherDriver) MostMentioned(fromDateEpoch int64, toDateEpoch int64, limit int) (thingList People, found bool, err error) {
---
+++ cypher_connected_people.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Financial-Times/neo-model-utils-go/mapper"
+	log "github.com/Sirupsen/logrus"
+	"github.com/jmcvetta/neoism"
+)
+
+type neoConnectedPeopleReadStruct struct {
+	UUID      string `json:"uuid"`
+	PrefLabel string `json:"prefLabel"`
+	Count     int    `json:"count"`
+}
+
+func (pcw CypherDriver) ConnectedPeople(uuid string, fromDateEpoch int64, toDateEpoch int64, limit int, minimumConnections int) (connectedPeople []ConnectedPerson, found bool, err error) {
+	results := []neoConnectedPeopleReadStruct{}
+
+	statement := `
+	MATCH (c:Content) where c.publishedDateEpoch < {toDate} and c.publishedDateEpoch > {fromDate}
+	WITH c
+	MATCH (p:Person{uuid:{uuid}})<-[:MENTIONS]-(c)-[:MENTIONS]->(p2:Person)
+	WITH p, count(distinct(c)) as cm, p2
+	WHERE cm > {minimumConnections}
+	WITH p2.uuid as uuid, p2.prefLabel as prefLabel, cm as count
+	RETURN prefLabel, uuid, count ORDER BY count DESC LIMIT {limit}`
+	//thing := Thing{}
+	query := &neoism.CypherQuery{
+		Statement: statement,
+		Parameters: neoism.Props{
+			"uuid":               uuid,
+			"fromDate":           fromDateEpoch,
+			"toDate":             toDateEpoch,
+			"minimumConnections": minimumConnections,
+			"limit":              limit,
+		},
+		Result: &results,
+	}
+	err = pcw.db.Cypher(query)
+	if err != nil {
+		log.Errorf(`Error finding people with more than %v connections to person with uuid %v
+      between %v and %v with the following statement: %v  Error: %v`, limit, uuid, fromDateEpoch, toDateEpoch, query.Statement, err)
+		return []ConnectedPerson{}, false, fmt.Errorf("Error finding people with more than %v connections to person with uuid %v between %v and %v with the following statement: %v  Error: %v", limit, uuid, fromDateEpoch, toDateEpoch, query.Statement, err)
+	}
+	log.Infof("CypherResult connectedPeople was (fromDate=%v, toDate=%v): %+v", fromDateEpoch, toDateEpoch, results)
+	if (len(results)) == 0 {
+		return []ConnectedPerson{}, false, nil
+	}
+
+	connectedPeopleResults := neoReadStructToConnectedPeople(&results, pcw.env)
+
+	log.Infof("Returning %v", connectedPeopleResults)
+	return connectedPeopleResults, true, nil
+}
+
+func neoReadStructToConnectedPeople(neo *[]neoConnectedPeopleReadStruct, env string) []ConnectedPerson {
+	connectedPeople := []ConnectedPerson{}
+	for _, neoCP := range *neo {
+		var connectedPerson = ConnectedPerson{}
+		connectedPerson.Person.APIURL = mapper.APIURL(neoCP.UUID, []string{"People"}, env)
+		connectedPerson.Person.ID = mapper.IDURL(neoCP.UUID)
+		connectedPerson.Person.PrefLabel = neoCP.PrefLabel
+		connectedPerson.Count = neoCP.Count
+		connectedPeople = append(connectedPeople, connectedPerson)
+	}
+	return connectedPeople
+}
