58a69fd5aec1e663736cc22b8bdf3ffad3686499
Nicky Wrightson <Nicky.Wrightson@ft.com>
2016-05-17 11:58:28 +0100

    Removed binary

--- cypher.go
+++ cypher.go
@@ -40,7 +40,7 @@ func (pcw CypherDriver) CheckConnectivity() error {
 type neoReadStruct struct {
 }
 
-func (pcw CypherDriver) MostMentioned(numberOfMostMentioned int64, timePeriod int64) (Thing, bool, error) {
+func (pcw CypherDriver) MostMentioned(numberOfMostMentioned int64, timePeriod int64'') (Thing, bool, error) {
 	thing := Thing{}
 	results := []struct {
 		Rs []neoReadStruct
