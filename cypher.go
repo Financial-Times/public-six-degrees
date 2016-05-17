a766584c2329a7bf15df1f4a05b636f8e2df2e3c
riyanat.shittu <riyanat.shittu@ft.com>
2016-05-17 17:33:31 +0100

    updates to reading people function

--- cypher.go
+++ cypher.go
@@ -40,44 +40,49 @@ func (pcw CypherDriver) CheckConnectivity() error {
 	return err
 }
 
-type neoReadStruct struct {
+type neoMentionsReadStruct struct {
 	UUID     string `json:"uuid"`
-	name     string `json:"name"`
-	mentions int    `json:mentions`
+	prefLabel   string `json:"prefLabel"`
+	mentions int    `json:"mentions"`
 }
 
 func (pcw CypherDriver) MostMentioned(fromDateEpoch int64, toDateEpoch int64, limit int) (thingList []Thing, found bool, err error) {
-	results := []neoReadStruct{}
+	log.Infof("logging fromDate:%v toDate:%v  limit:%v",fromDateEpoch, toDateEpoch, limit)
+	results := []neoMentionsReadStruct{}
 	query := &neoism.CypherQuery{
 		Statement: `MATCH (c:Content)-[a:MENTIONS]->(p:Person)
 					WHERE c.publishedDateEpoch > {fromDateEpoch} AND c.publishedDateEpoch < {toDateEpoch}
-					WITH p.prefLabel as name, p.uuid as uuid,
+					WITH p.prefLabel as prefLabel, p.uuid as uuid,
 					COUNT(a) as mentions
-					RETURN name, uuid, mentions
+					RETURN uuid, prefLabel, mentions
 					ORDER BY mentions
 					DESC LIMIT {mentionsLimit}`,
 		Parameters: neoism.Props{"fromDateEpoch": fromDateEpoch, "toDateEpoch": toDateEpoch, "mentionsLimit": limit},
 		Result:     &results,
 	}
+	log.Infof("Query %v", query)
 
 	err = pcw.db.Cypher(query)
+	log.Infof("Results%v", &results)
+
 	if err != nil {
 		log.Errorf("Error finding %v most mentioned people between %v and %v with the following statement: %v  Error: %v", limit, fromDateEpoch, toDateEpoch, query.Statement, err)
 		return []Thing{}, false, fmt.Errorf("Error finding %v most mentioned people between %v and %v", limit, fromDateEpoch, toDateEpoch)
 	}
 	log.Debugf("CypherResult MostMentioned was (fromDate=%v, toDate=%v): %+v", limit, fromDateEpoch, toDateEpoch, results)
 
-	thingList, _ = neoReadStructToThing(&results, pcw.env)
-	log.Debugf("Returning %v", thingList)
+	thingList, _ = neoReadStructToMentionPeople(&results, limit, pcw.env)
+	log.Infof("Returning %v", thingList)
 	return thingList, true, nil
 }
 
-func neoReadStructToThing(neo *[]neoReadStruct, env string) (peopleList []Thing, err error) {
-	peopleList = make([]Thing, len(*neo))
+func neoReadStructToMentionPeople(neo *[]neoMentionsReadStruct, limit int, env string) (peopleList []Thing, err error) {
+	peopleList = make([]Thing, limit + 1)
 	for _, neoCon := range *neo {
+		log.Infof("neoCon result: %v", neoCon)
 		var thing = Thing{}
 		thing.ID = mapper.IDURL(neoCon.UUID)
-		thing.PrefLabel = neoCon.name
+		thing.PrefLabel = neoCon.prefLabel
 		peopleList = append(peopleList, thing)
 	}
 	return peopleList, nil
