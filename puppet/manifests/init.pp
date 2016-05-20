a6dae84dd12b744a81425cab92da0e3986afbfe9
Nicky Wrightson <Nicky.Wrightson@ft.com>
2016-05-20 10:57:54 +0100

    REmoved api at the end

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,10 +1,10 @@
-class public_six-degrees_api {
+class public_six-degrees {
 
   $configParameters = hiera('configParameters','')
 
   class { "go_service_profile" :
     service_module => $module_name,
-    service_name => 'public-six-degrees-api',
+    service_name => 'public-six-degrees',
     configParameters => $configParameters
   }
 
